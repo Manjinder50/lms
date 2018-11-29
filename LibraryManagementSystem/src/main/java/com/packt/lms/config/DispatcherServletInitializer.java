52867fd986490d8d8d5d65024c52dbc2d031466e
Manjinder <Manjinder@LAPTOP-AVOI82AG>
2018-11-29 22:33:27 +0530

    Added basic structure for library management system using spring

---
+++ LibraryManagementSystem/src/main/java/com/packt/lms/config/DispatcherServletInitializer.java
@@ -0,0 +1,25 @@
+package com.packt.lms.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] {WebApplicationContextConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+
+		return new String[] {"/"};
+	}
+
+}
